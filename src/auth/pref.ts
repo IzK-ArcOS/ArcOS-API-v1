921766265888921d19a441c83ee15c2df12bc4f6
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-01-15 23:35:58 +0100

    Add DevMode user property

--- src/auth/pref.ts
+++ src/auth/pref.ts
@@ -27,6 +27,7 @@ export interface UserPreferences {
 
   disabledApps: string[];
   autoRun: string[];
+  devmode: boolean;
 
   acc: {
     enabled: boolean;
@@ -73,4 +74,5 @@ export const DefaultUserdata: UserPreferences = {
   },
   disabledApps: [],
   autoRun: [],
+  devmode: false,
 };
