1468cbd6969da3a08bea9ddffe9f74bf21a04448
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-03-26 16:40:32 +0200

    Update `/connect`

--- src/api/arcos/connect.ts
+++ src/api/arcos/connect.ts
@@ -1,13 +1,16 @@
 import { IncomingMessage, ServerResponse } from "http";
 import url from "url";
+import { CONFIG } from "../../config/store";
 import { Ok } from "../../server/return";
 
 export async function ArcOSConnect(req: IncomingMessage, res: ServerResponse) {
+  console.log(url.parse(req.url as string, true));
   Ok(
     res,
     JSON.stringify({
-      platform: "ArcOS",
-      hostname: url.parse(req.url as string, true).hostname,
+      platform: `ArcOS @ ${CONFIG.name}`,
+      port: CONFIG.port,
+      referrer: url.parse(req.url as string).pathname,
       valid: true,
     })
   );
