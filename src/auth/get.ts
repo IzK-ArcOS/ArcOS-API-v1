7c2ae2f12fbc1fcf44ae6248c8d3fc6ef9fa0606
Izaak Kuipers <izaak.kuipers@gmail.com>
2023-07-01 22:24:15 +0200

    Resolve #4

--- src/auth/get.ts
+++ src/auth/get.ts
@@ -2,13 +2,14 @@ import { IncomingMessage } from "http";
 
 export function getAuth(req: IncomingMessage) {
   const header = req.headers.authorization;
-
   const token = (header || "").split(/\s+/).pop() || "";
-
   const auth = Buffer.from(token, "base64").toString();
   const parts = auth.split(/:/);
   const username = parts.shift()?.trim() || "";
   const password = parts.join(":").trim();
 
-  return { username, password };
+  return {
+    username: username.substring(0, Math.min(username.length, 25)),
+    password,
+  };
 }
